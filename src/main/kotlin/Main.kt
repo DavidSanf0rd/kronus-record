a940e1a653505afe3bbd520862b22b54c636531b
David Sanford <dsanfordseu@gmail.com>
2017-06-19 21:37:51 -0300

    Solve the reflection problem

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,21 @@
+import kotlin.reflect.full.declaredMemberProperties
+
 /**
  * Created by sanf0rd on 19/06/17.
  */
+
+fun readProperties(instance: Any) {
+    val clazz = instance.javaClass.kotlin
+    clazz.declaredMemberProperties.forEach {
+        println("${it.name} --[${it.returnType}]---> ${it.get(instance)}")
+    }
+}
+
+data class MyData(val name: String, val age: Int)
+
 fun main(args: Array<String>) {
-    print("Initial file")
+
+    val data = MyData(name = "Sanford", age = 19)
+
+    readProperties(data)
 }
