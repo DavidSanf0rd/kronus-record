5258d9df27d965d6d9ff1e21aecd8c5b113e2ed0
David Sanford <dsanfordseu@gmail.com>
2017-07-10 16:20:20 -0300

    Connect with jdbc driver

--- src/main/kotlin/KronusOrm.kt
+++ src/main/kotlin/KronusOrm.kt
@@ -1,4 +1,6 @@
+import Helpers.OperationalSystem
 import java.sql.Connection
+import java.sql.DriverManager
 
 /**
  * Created by sanf0rd on 09/07/17.
@@ -12,10 +14,38 @@ class KronusOrm() {
 
     constructor(databaseName: String): this() {
         this.databaseName = databaseName
+        connect()
     }
 
     constructor(databaseName: String, databasePath: String) : this() {
         this.databaseName = databaseName
         this.databasePath = databasePath
+
+        connect()
+    }
+
+    fun connect() {
+        var databaseName = databaseName?.let { it } ?: return //trow an error in future
+
+        if (databaseName.endsWith(".db")) {
+            databaseName = "$databaseName.db"
+            this.databaseName = databaseName
+        }
+
+        if (databasePath.isNullOrEmpty()) {
+            databasePath = OperationalSystem.homeDir
+        }
+
+        val dbUrl: String
+
+        if(OperationalSystem.isAndroid()) {
+            Class.forName("org.sqldroid.SQLDroidDriver")
+            dbUrl = "jdbc:sqldroid:$databasePath/$databaseName"
+        } else {
+            Class.forName("org.sqlite.JDBC")
+            dbUrl = "jdbc:sqlite:$databasePath/$databaseName"
+        }
+        
+        connection = DriverManager.getConnection(dbUrl)
     }
 }
