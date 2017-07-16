2d1613da5da7c4749691f7356cb965822615ca16
David Sanford <dsanfordseu@gmail.com>
2017-07-16 00:36:57 -0300

    Fix allFrom DBReader method signature

--- src/main/kotlin/Core/interfaces/DBReader.kt
+++ src/main/kotlin/Core/interfaces/DBReader.kt
@@ -6,5 +6,5 @@ import kotlin.reflect.KClass
  * Created by Sanf0rd on 15/07/17.
  */
 interface DBReader: DBOperator {
-    fun allFrom(entity: KClass<DBEntity>)
+    fun allFrom(entity: KClass<DBEntity>): List<DBEntity>
 }
