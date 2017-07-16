60e04a39d0c73bbf6de6f28c6c99c9c8b86b4624
Victor <victoralissont5@gmail.com>
2017-07-16 17:28:29 -0300

    Add generic delete method

--- src/main/kotlin/Deletable.kt
+++ src/main/kotlin/Deletable.kt
@@ -1,8 +1,10 @@
+import Core.interfaces.DBEntity
+
 /**
  * Created by victoralisson on 19/06/17.
  */
 
-interface Deletable<T: DataModel> {
+interface Deletable<T: DataModel>: Referable, DBEntity {
     fun delete() {
         println("Deletando usuário")
     }
